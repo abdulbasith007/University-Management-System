2dc32074bc1c1af91d17383c437577fd7ef6b05e
Basith Abdul <abdulbasithCOC@gmail.com>
2024-11-10 01:09:14 -0500

    Added controller for rendering index, landing and error404 page

---
+++ controllers/home.js
@@ -0,0 +1,12 @@
+exports.getIndex = (req, res, next) => {
+    res.render('index');
+  };
+  
+  exports.getLanding = (req, res, next) => {
+    res.render('landing');
+  }
+  
+  exports.getError404 = (req, res, next) => {
+    res.render('error404');
+  }
+  
